72a40b087177df13d9f6d74a3e4e0c8d0182c26f
Joshua Ray <superdog1695@yahoo.com>
2021-05-18 18:53:48 -0400

    Store log timestamp to increase efficiency and accuracy

--- Services/LogService.cs
+++ Services/LogService.cs
@@ -11,7 +11,8 @@ namespace PokerBot.Services
     
         private static string GetTimestamp()
         {
-            return $"{DateTime.Now.Hour:D2}:{DateTime.Now.Minute:D2}:{DateTime.Now.Second:D2}";
+            var currentTime = DateTime.Now;
+            return $"{currentTime.Hour:D2}:{currentTime.Minute:D2}:{currentTime.Second:D2}";
         }
     }
 }
