db3d321176689b783d56e1b43be0613635de4c84
Joshua Ray <superdog1695@yahoo.com>
2021-05-24 21:07:04 -0400

    Added Dapper and MySQL nuget packages. Also added a basic SQL service for accessing the database

--- Program.cs
+++ Program.cs
@@ -41,9 +41,15 @@ namespace PokerBot
             await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
             await _client.StartAsync();
 
+            var env = Environment.GetEnvironmentVariables();
             _services = new ServiceCollection()
                 .AddSingleton(_client)
                 .AddSingleton(new PokerService())
+                .AddSingleton(new SqlService($"server ={ env["DB_SERVER"] }; " +
+                    $"port={env["DB_PORT"]};" +
+                    $"database={env["DB_NAME"]};" +
+                    $"user={env["DB_USER"]};" +
+                    $"password={env["DB_PASS"]}"))
                 .BuildServiceProvider();
 
             _commands = new CommandService();
---
+++ Services/SqlService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using PokerBot.Classes;
+
+namespace PokerBot.Services
+{
+    class SqlService
+    {
+        private readonly MySqlConnection connection;
+
+        public SqlService(string connectionString)
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public async Task<PokerPlayer> GetPlayerAsync(ulong id)
+        {
+            connection.Open();
+
+            return await connection.QuerySingleAsync<PokerPlayer>("SELECT * FROM player WHERE discordId = @id", id);
+        }
+    }
+}
